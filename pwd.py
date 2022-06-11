809a20441a8879225c102d7aeae5318512d1ff13
Chia-Tzu Lin <greenlivingbrilliant@gmail.com>
2022-06-11 17:11:42 +0800

    final version

--- pwd.py
+++ pwd.py
@@ -1,14 +1,15 @@
 password = 'a123456' 
 n = 3
 
-while True:
+while n > 0:
+	n = n - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password :
 		print ('登入成功!')
 		break 
 	else:
-		n = n - 1
-		print('密碼錯誤! 還有 ', n , '次機會')
-		if n == 0:
-			break
-			
+		print('密碼錯誤!')
+		if n > 0:
+			print('還有' , n , '次機會')
+		else:
+			print('沒機會嘗試了!要鎖帳號囉!')
